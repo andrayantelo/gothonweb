b9b3a1e788c52db31afc4e380f4a83416a41a6c2
Andrea Anaya <andrayantelo@gmail.com>
2015-04-30 15:54:55 -0500

    fixed syntax errors

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ try:
 except ImportError:
     from distutils.core import setup
     
-config = [
-    'description': 'My Project',
-    'author': 'Andrea Anaya',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'My email.',
-    'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': ['gothonweb'],
-    'scripts': [],
-    'name': 'gothonweb'
-]
+setup(
+    description= 'My Project',
+    author= 'Andrea Anaya',
+    url= 'URL to get it at.',
+    download_url= 'Where to download it.',
+    author_email= 'My email.',
+    version= '0.1',
+    install_requires= ['nose'],
+    packages= ['gothonweb'],
+    scripts= ['sample.py'],
+    name= 'gothonweb'
+)
 
 setup(**config)
     
