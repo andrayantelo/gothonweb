5e8634f8f59ba8174552040a585f1f6509a578fb
Andrea Anaya <andrayantelo@gmail.com>
2015-05-07 20:39:14 -0500

    added one test

--- tests/map_tests.py
+++ tests/map_tests.py
@@ -31,3 +31,9 @@ def test_map():
     assert_equal(start.go('west').go('east'), start)
     assert_equal(start.go('down').go('up'), start)
     
+def test_gothon_game_map():
+    assert_equal(START.go('shoot!'), generic_death)
+    assert_equal(START.go('dodge!'), generic_death)
+    
+    room = START.go('tell a joke')
+    assert_equal(room, laser_weapon_armory)
